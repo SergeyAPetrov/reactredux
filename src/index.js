aeb716fe110f7284730049c06f4edf779452ad50
Sergey Petrov <sergeyp@confirmit.com>
2018-05-04 16:55:37 +0300

    some pages

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
-console.log(2);
+import 'babel-polyfill';
+import React from 'react';
+import {render} from 'react-dom';
+import routes from './routes';
+import {Router, browserHistory} from 'react-router';
+import './styles/styles.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+
+render(
+    <Router history={browserHistory} routes={routes} />,
+    document.getElementById('app')
+);
